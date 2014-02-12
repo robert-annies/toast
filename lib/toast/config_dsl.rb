eb63ae5ce2c4ec8dbc6c1765123ed9b04a615bbb
robokopp <robokopp@fernwerk.net>
2014-02-12 13:21:10 +0100

    inline api documentation (experimental)
    The field configuarations (readables/writables) allow to add comments
    for the fields. As for now the comments can only be retrieved directly
    from Model.toast_config(media_type).apidoc.
    
    Experimental, incomplete and undocumented feature.

--- lib/toast/config_dsl.rb
+++ lib/toast/config_dsl.rb
@@ -9,6 +9,7 @@ module Toast
         @model = model
         @readables = []
         @writables = []
+        @field_comments = {}
         @collections = []
         @singles = []
         @deletable = false
@@ -16,6 +17,7 @@ module Toast
         @pass_params_to = []
         @in_collection = ConfigDSL::InCollection.new model, self
         @media_type = "application/json"
+        @apidoc = {}
 
         @model.attr_accessible []
       end
@@ -31,7 +33,8 @@ module Toast
       end
 
       def readables= arg
-          @readables.push *ConfigDSL.sanitize(arg,"readables")
+        @field_comments.merge! ConfigDSL.get_comments(arg, 'ro')
+        @readables.push *ConfigDSL.normalize(arg,"readables")
       end
 
       # args: Array or :all, :except => Array
@@ -42,7 +45,8 @@ module Toast
 
       def writables= arg
         @model.attr_accessible *arg
-        @writables.push *ConfigDSL.sanitize(arg,"writables")
+        @field_comments.merge! ConfigDSL.get_comments(arg, 'rw')
+        @writables.push *ConfigDSL.normalize(arg,"writables")
       end
 
       # args: Array or :all, :except => Array
@@ -68,7 +72,7 @@ module Toast
       end
 
       def pass_params_to= arg
-        @pass_params_to.push *ConfigDSL.sanitize(arg,"pass_params_to")
+        @pass_params_to.push *ConfigDSL.normalize(arg,"pass_params_to")
       end
 
       def pass_params_to *arg
@@ -77,7 +81,7 @@ module Toast
       end
 
       def collections= collections=[]
-        @collections = ConfigDSL.sanitize(collections, "collections")
+        @collections = ConfigDSL.normalize(collections, "collections")
       end
 
       def collections *arg
@@ -86,7 +90,7 @@ module Toast
       end
 
       def singles= singles=[]
-        @singles = ConfigDSL.sanitize(singles, "singles")
+        @singles = ConfigDSL.normalize(singles, "singles")
       end
 
       def singles *arg
@@ -102,8 +106,18 @@ module Toast
         end
       end
 
+      def apidoc arg=nil
+        return(@apidoc) if arg.nil?
+        raise "Toast Config Error (#{model.class}): apidoc argument must be a Hash." unless arg.is_a?(Hash)
+        @apidoc = arg
+      end
+
       # non DSL methods
       dsl_methods false
+
+      def field_comments
+        @field_comments
+      end
     end
 
     class InCollection
@@ -120,7 +134,7 @@ module Toast
 
       def readables= readables
         @writables = [] # forget inherited writables
-        @readables = ConfigDSL.sanitize(readables,"readables")
+        @readables = ConfigDSL.normalize(readables,"readables")
       end
 
       def readables *arg
@@ -158,26 +172,51 @@ module Toast
         (@readables + @writables).uniq.select{|f| assocs.include?(f)}
       end
 
-    end
-
+    end # class InCollection
 
     # Helper
 
     # checks if list is made of symbols and strings
     # converts a single value to an Array
     # converts all symbols to strings
-    def self.sanitize list, name
+    def self.normalize list, name
+      # make single element list
+      list = [list] unless list.is_a? Array
+
+      # remove all comments
+      list = list.map{|x| x.is_a?(Array) ? x.first : x}
+
+      # flatten
       list = [list].flatten
 
+      # check class and stringify
       list.map do |x|
         if (!x.is_a?(Symbol) && !x.is_a?(String))
-          raise "Toast Config Error: '#{name}' should be a list of Symbols or Strings"
+          raise "Toast Config Error: '#{name}' should be a list of Symbols"
         else
           x.to_s
         end
       end
     end
 
+    # fields (readables and writables) can have comments , if passed by Arrays of the form:
+    # [symbol, comment]
+    # returns a Hash of all commented or uncommented fields as:
+    # { FIELD_NAME => {:comment => COMMENT, :type => type } }
+    def self.get_comments arg, access
+      comments = {}
 
+      if arg.is_a? Array
+        arg.each do |f|
+          if f.is_a? Array
+            comments[ f.first.to_s ] = {:comment => f[1].to_s, :access => access, :type => f[2]}
+          else
+            comments[ f.to_s ] = {:comment => '[ no comment ]', :access => access, :type => nil}
+          end
+        end
+      end
+
+      comments
+    end
   end
 end
