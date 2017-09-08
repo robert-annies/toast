f0965a748a489413070beaa088bbdc5689ff001c
robokopp <robokopp@fernwerk.net>
2017-09-08 13:25:47 +0200

    merge troubles

--- lib/toast/version.rb
+++ lib/toast/version.rb
@@ -1,41 +1,4 @@
 module Toast
-<<<<<<< 60adfdf97acd4908f5c04bd9b99a16d404dcabeb
-<<<<<<< 21bf1f3c931941abb8bc9912c8fe3e844d60bbdf
-<<<<<<< 97e12482dc6e6626e34ef922cff823af0c609ba5
-<<<<<<< 8f4d23f227418b8b8c96afc8cc36476ff4cd7c45
-<<<<<<< 542ebf55a00dfc64d12f017b20b571bc14cf8b4a
-=======
-<<<<<<< b9e44993da49a6d2306b05f1ccae17880cd532cf
->>>>>>> Toast version 1.0.0
   VERSION = '1.0.0'
-=======
-  VERSION = '1.0.0.beta'
->>>>>>> Toast version 1.0.0
-<<<<<<< 60adfdf97acd4908f5c04bd9b99a16d404dcabeb
-=======
-=======
-<<<<<<< e0052079b2b5f08da4195c977af51b1f3d8a82d9
->>>>>>> Toast version 1.0.0
-<<<<<<< 6406c83dab62670d95b379af2b3e56b83c3d063c
-  VERSION = '1.0.0.beta'
-=======
-  VERSION = '1.0.0'
->>>>>>> # This is a combination of 2 commits.
-<<<<<<< 97e12482dc6e6626e34ef922cff823af0c609ba5
->>>>>>> # This is a combination of 2 commits.
-=======
-=======
-<<<<<<< 0e2a80e65d74ac131dd390bba591974c5c15c4e0
-  VERSION = '1.0.0'
-=======
-  VERSION = '1.0.0.beta'
->>>>>>> Toast version 1.0.0
->>>>>>> Toast version 1.0.0
->>>>>>> Toast version 1.0.0
-=======
-  VERSION = '1.0.0'
->>>>>>> bugfix: virtual attribute setter should not be called when attribute omitted in payload
-=======
->>>>>>> Toast version 1.0.0
 end
  
