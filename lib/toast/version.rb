97e12482dc6e6626e34ef922cff823af0c609ba5
robokopp <robokopp@fernwerk.net>
2017-09-08 12:15:02 +0200

    # This is a combination of 2 commits. # The first commit's message is:
    restructure test app tree for rails 4 test suite
    
    leading slash in test requests required in newer rails
    
    :via required in routes
    
    use strong parameters in rails 4
    
    various changes for rails 4 compatibility
    
    rails 4 test application
    
    no seeds necessary
    
    ignore rbenv's ruby-version
    
    enforce compatible gem versions
    
    skip test when rbenv not found
    
    skip testing earlier when gems not installable
    
    bugfix, show failure in summary log
    
    Better call Saul!
    
    version bump
    
    # This is the 2nd commit message:
    
    README update

--- lib/toast/version.rb
+++ lib/toast/version.rb
@@ -1,8 +1,16 @@
 module Toast
+<<<<<<< 8f4d23f227418b8b8c96afc8cc36476ff4cd7c45
 <<<<<<< 542ebf55a00dfc64d12f017b20b571bc14cf8b4a
   VERSION = '1.0.0'
 =======
   VERSION = '1.0.0.beta'
 >>>>>>> Toast version 1.0.0
+=======
+<<<<<<< 6406c83dab62670d95b379af2b3e56b83c3d063c
+  VERSION = '1.0.0.beta'
+=======
+  VERSION = '1.0.0'
+>>>>>>> # This is a combination of 2 commits.
+>>>>>>> # This is a combination of 2 commits.
 end
  
