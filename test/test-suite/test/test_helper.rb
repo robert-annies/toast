11c98b4d273302296778c42133b303d88d93b802
Robert Anniés <robert.annies@insel.ch>
2019-02-20 10:54:40 +0100

    removes mini-test reporters

--- test/test-suite/test/test_helper.rb
+++ test/test-suite/test/test_helper.rb
@@ -1,21 +1,6 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require 'minitest/reporters'
-
-# wider padding of test names
-#   ovveride in gem minitest-reporters
-#   see https://github.com/kern/minitest-reporters/blob/master/lib/minitest/relative_position.rb
-
-module Minitest
-  module RelativePosition
-    def pad_test(str)
-      pad("%-100s" % str[5..-1], TEST_PADDING)
-    end
-  end
-end
-
-Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
@@ -30,10 +15,10 @@ module ActionDispatch::Integration::RequestHelpers
   [:get,:put,:patch,:post,:delete,:link,:unlink].each do |method_name|
     define_method method_name do |path, *args|
       case Rails.version
-      when /\A4\./   
+      when /\A4\./
         process method_name, path, args.first.try(:fetch,:params, nil), args.first.try(:fetch,:headers, nil)
       when /\A5\.0\./
-        process_with_kwargs(method_name, path, *args) 
+        process_with_kwargs(method_name, path, *args)
       else  # >= 5.1
         process method_name, path, *args
       end
